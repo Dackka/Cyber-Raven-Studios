d0932f2c7e6f479949d34dd2bdbe0afd36950989
Caleb_101123 <136540914+Caleb101123@users.noreply.github.com>
2023-06-20 18:05:51 -0400

    Scene Transition Added

---
+++ Cardiac Arrest/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class SceneTransition : MonoBehaviour
+{
+	
+	//Variable to determine scene to go to.
+	[Header("Private Vars")]
+	[SerializeField] private string target;
+    [SerializeField] private Collider2D player;
+	
+	private void Start()
+	{
+		player = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+	}
+	
+	private void OnTriggerStay2D (Collider2D enter)
+    {
+		if (enter == player)
+        {
+			SceneManager.LoadScene(target);
+		}
+    }
+}
